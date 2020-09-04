6a03e3e288e65a432c2dd58800015e8a79ac0b21
Rafael Durán <duran.r.m.g@gmail.com>
2020-09-03 22:39:41 -0300

    Solución ejercicio 1

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,28 +4,14 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
-
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             /*Este método debería ir en una clase que se dedique a administrar dinero, porque no cumple con el criterio SRP.
             El cálculo del precio del alfajor no cumple con el criterio expert, ya que sería la clase Alfajor la que debería encargarse de esta función */
-            Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
+            Double pesos = Dinero.ConvertirAPesos(dinero, moneda);
+            //return pesos >= a.PrecioDulce + a.PrecioMasa;//
+            return pesos >=a.PrecioFinal();
         }
-        private Double ConvertirAPesos(Double dinero, String moneda)
-        {
-            if (moneda.Equals("U$S")) 
-            {
-                return dinero / 30;
-            } 
-            else if (moneda.Equals("$")) 
-            {
-                return dinero;
-            }
-            else 
-            {
-                return -1;
-            }
-        }
+        
     }
 }
