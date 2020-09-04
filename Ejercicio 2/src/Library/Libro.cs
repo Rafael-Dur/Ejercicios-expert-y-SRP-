92140b96289702f06be30f94ac9a01a40e199c92
Rafael Durán <duran.r.m.g@gmail.com>
2020-09-03 23:25:15 -0300

    Comentarios ejercicio 2

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -4,10 +4,10 @@ namespace SRP
 {
     public class Libro
     {
-
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
+
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
@@ -17,12 +17,13 @@ namespace SRP
             this.Autor = autor;
             this.Codigo = codigo;
         }
-
+        /*La responsabilidad de almacenar los libros no recae en el libro, 
+        ya que este no tiene la necesidad de conocer su posición
+        Por lo tanto, debe ir en una nueva clase (libreria)*/
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
         }
-
     }
 }
