76ff98ee6ff7b1732e2f6d7fd85657be9de80654
Rafael Durán <duran.r.m.g@gmail.com>
2020-09-04 00:11:26 -0300

    Comentarios ejercicio 3

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -5,6 +5,12 @@ namespace Library
 {
     public class AppointmentService
     {
+        /*No le corresponde a la clase AppointmentService la responsabilidad de verificar
+        si los datos son válidos o no. Esta responsabilidad debería ir en otra clase experta en ese campo
+        
+        No se está cumpliendo con el patrón SRP, ya que el módulo tiene más de un motivo para cambiar, debido a que si
+        se desea modificar la forma de verificar algún dato o la cantidad de datos requeridos para crear la cita, se tendría
+        que modificar esta clase.*/
         public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
@@ -34,7 +40,6 @@ namespace Library
                 isValid = false;
             }
 
-            
             if (string.IsNullOrEmpty(doctorName))
             {
                 stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
@@ -48,6 +53,5 @@ namespace Library
 
             return stringBuilder.ToString();
         }
-
     }
 }
